c443660e0d8f956e2c8140faacefb6cfc70714c0
Andrews Esteban <andrewsesteban7@gmail.com>
2023-08-15 09:27:38 -0500

    addgin page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Page_O from './pageOleada/Page_O';
+import Home  from './homePage/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/Page_O">Page oleada</Link>
+        </nav>
+        <Route path="/" exact component={Home} />
+        <Route path="/Page_O" exact component={Page_O} />
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pageOleada/Page_O.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page_o = () => {
+  return (
+    <div>Oleada 1 Texto</div>
+  )
+}
+
+export default Page_o
