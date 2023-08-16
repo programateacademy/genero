3722228f2282e951534720a93d44c1b75038ad95
bccastro <bhcastro1101@gmail.com>
2023-08-16 18:02:30 -0500

    creo componente card

---
+++ src/pageWave/Components/Books/Cards.jsx
@@ -0,0 +1,16 @@
+import React from 'reactr'
+import {Card, cardmedia} from '@mui/material'
+
+const Cards = () => {
+    return Cards = (
+        <div>
+            <Card sx= {{maxwidth:300}}>
+             <cardmedia component={'img'}  
+             height="140"
+img=''
+            </Card>
+
+        </div>
+    )
+}
+
