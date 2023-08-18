a5b6926628a3e86bebbe68a90f331bc003e9f03e
Andrews Esteban <andrewsesteban7@gmail.com>
2023-08-18 17:55:54 -0500

    merge andrews-Karol

---
+++ src/pageWave/pageWave.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import WaveAnimation from './Components/Section/WaveAnimation';
+import Container from './Components/Section/Container';
+
+const Page_o = () => {
+  return (
+    <div>
+         <Container/>
+         <WaveAnimation/>
+    </div>
+  )
+}
+
+export default Page_o
