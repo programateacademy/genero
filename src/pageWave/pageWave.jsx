1e8ad5d971ff26123bf64fbb1e5f3ed566e067fc
bccastro <bhcastro1101@gmail.com>
2023-08-17 22:47:24 -0500

    cards created for wave 1

--- src/pageWave/pageWave.jsx
+++ src/pageWave/pageWave.jsx
@@ -3,14 +3,40 @@ import ImageBook from './Components/Books/ImageBook'
 import Description from './Components/Books/Description'
 import Button from './Components/Books/Button'
 import Visualization from './Components/Books/Visualization'
+import Card from './Components/Cards'
+
 
 const Page_o = () => {
   return (
     <div>
-      <ImageBook/>
-      <Description/>
-      <Button/>
-      <Visualization/>
+      <ImageBook />
+      <Description />
+      <Button />
+      <Visualization />
+
+      <div className="card-group">
+
+        <Card title="¿Quiénes lideran el movimiento Feminista Chicano?"
+          description="El movimiento feminista chicano fue liderado por muchas mujeres notables, incluyendo a Dolores Huerta, Cherríe Moraga, Gloria Anzaldúa, Ana Castillo, Sandra Cisneros, y muchas más."
+          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg"/>
+
+        <Card title="¿Quiénes lideran el movimiento Feminista Chicano?"
+          description="El movimiento feminista chicano fue liderado por muchas mujeres notables, incluyendo a Dolores Huerta, Cherríe Moraga, Gloria Anzaldúa, Ana Castillo, Sandra Cisneros, y muchas más."
+          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg"/>
+      </div>
+
+      <div className="card-group">
+        
+        <Card title="¿Quiénes lideran el movimiento Feminista Chicano?"
+          description="El movimiento feminista chicano fue liderado por muchas mujeres notables, incluyendo a Dolores Huerta, Cherríe Moraga, Gloria Anzaldúa, Ana Castillo, Sandra Cisneros, y muchas más."
+          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg"/>
+
+        <Card title="¿Quiénes lideran el movimiento Feminista Chicano?"
+          description="El movimiento feminista chicano fue liderado por muchas mujeres notables, incluyendo a Dolores Huerta, Cherríe Moraga, Gloria Anzaldúa, Ana Castillo, Sandra Cisneros, y muchas más."
+          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg"/>
+
+      </div>
+
     </div>
   )
 }
