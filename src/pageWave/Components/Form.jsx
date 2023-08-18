616e817f05cf95add26252bb98dac38f75ce260d
amSebas <sebaslgr@gmail.com>
2023-08-17 20:49:28 -0500

    adding form with styles

--- src/pageWave/Components/Form.jsx
+++ src/pageWave/Components/Form.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import '../style.css'
 
 const Form = () => {
+
   return (
     <section className='form'>
       <div className='grid-content'>
         <div className='text'>
-          <h2>¡Tu voz importa y queremos escucharte!</h2>
+          <h1 className='white'>¡Tu voz importa y queremos escucharte!  </h1>
+          <h1 className='purple'>Unete a nosotras</h1>
+        </div>
+        <div className='grid-input'>
+          <div className='content-input'>
+            <p>Tu nombre</p>
+            <input type="text" name='name' className='name' placeholder='Name' />
+          </div>
+          <div className='content-input'>
+            <p>Tu correo</p>
+            <input type="text" name='email' className='email' placeholder='Email' />
+          </div>
+          <div className='content-input'>
+            <p>Tu mensaje</p>
+            <textarea type="text" name='message' className='message' />
+            <button type='submit' className='btn-submit'>Enviar</button>
+          </div>
         </div>
       </div>
     </section>
