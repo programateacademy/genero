8b1f4128caab148bc82ad73f602c289729efd246
bccastro <bhcastro1101@gmail.com>
2023-08-17 22:49:14 -0500

    pagewave modification

--- src/pageWave/pageWave.jsx
+++ src/pageWave/pageWave.jsx
@@ -16,24 +16,28 @@ const Page_o = () => {
 
       <div className="card-group">
 
-        <Card title="¿Quiénes lideran el movimiento Feminista Chicano?"
+        <Card 
+        title="¿Quiénes lideran el movimiento Feminista Chicano?"
           description="El movimiento feminista chicano fue liderado por muchas mujeres notables, incluyendo a Dolores Huerta, Cherríe Moraga, Gloria Anzaldúa, Ana Castillo, Sandra Cisneros, y muchas más."
-          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg"/>
+          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg" />
 
-        <Card title="¿Quiénes lideran el movimiento Feminista Chicano?"
+        <Card 
+        title="¿Quiénes lideran el movimiento Feminista Chicano?"
           description="El movimiento feminista chicano fue liderado por muchas mujeres notables, incluyendo a Dolores Huerta, Cherríe Moraga, Gloria Anzaldúa, Ana Castillo, Sandra Cisneros, y muchas más."
-          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg"/>
+          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg" />
       </div>
 
       <div className="card-group">
-        
-        <Card title="¿Quiénes lideran el movimiento Feminista Chicano?"
-          description="El movimiento feminista chicano fue liderado por muchas mujeres notables, incluyendo a Dolores Huerta, Cherríe Moraga, Gloria Anzaldúa, Ana Castillo, Sandra Cisneros, y muchas más."
-          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg"/>
 
-        <Card title="¿Quiénes lideran el movimiento Feminista Chicano?"
+        <Card 
+        title="¿Quiénes lideran el movimiento Feminista Chicano?"
           description="El movimiento feminista chicano fue liderado por muchas mujeres notables, incluyendo a Dolores Huerta, Cherríe Moraga, Gloria Anzaldúa, Ana Castillo, Sandra Cisneros, y muchas más."
-          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg"/>
+          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg" />
+
+        <Card 
+        title="¿Quiénes lideran el movimiento Feminista Chicano?"
+          description="El movimiento feminista chicano fue liderado por muchas mujeres notables, incluyendo a Dolores Huerta, Cherríe Moraga, Gloria Anzaldúa, Ana Castillo, Sandra Cisneros, y muchas más."
+          imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg" />
 
       </div>
 
