a224dd6334af8154fb2e88fe5284d3fd3ed49c9b
Andrews Esteban <andrewsesteban7@gmail.com>
2023-08-17 20:52:30 -0500

    merge andrews bryant

--- src/Page/pageWave/pageWave.jsx
+++ src/Page/pageWave/pageWave.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import ImageBook from './Components/Books/ImageBook'
-import Description from './Components/Books/Description'
-import Button from './Components/Books/Button'
 import MyBook from './Components/Books/MyBook'
 import './style.css'
+import Card from './Components/Card'
+
 
 const Page_o = () => {
   return (
     <div>
-      <ImageBook/>
-      <Description/>
-      <Button/>
+        <Card
+        title="¿Quiénes lideran el movimiento Feminista Chicano?"
+        description="El movimiento feminista chicano fue liderado por muchas mujeres notables, incluyendo a Dolores Huerta, Cherríe Moraga, Gloria Anzaldúa, Ana Castillo, Sandra Cisneros, y muchas más."
+        imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg"
+      />
       <MyBook/>
     </div>
   )
