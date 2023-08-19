e818025c4e4211afaac5e9ddf8c43cbc6bc3e807
bccastro <bhcastro1101@gmail.com>
2023-08-18 21:01:48 -0500

    commit 4

--- src/Page/pageWave/pageWave.jsx
+++ src/Page/pageWave/pageWave.jsx
@@ -10,7 +10,7 @@ const Page_o = () => {
 
       <div className="card-group">
         <Card 
-          title="¿Quiénes lideran el movimiento Feminista Chicano?"
+          title="¿Quiénes son los locos del codigo?"
           description="El movimiento feminista chicano fue liderado por muchas mujeres notables, incluyendo a Dolores Huerta, Cherríe Moraga, Gloria Anzaldúa, Ana Castillo, Sandra Cisneros, y muchas más."
           imageUrl="https://d1ih8jugeo2m5m.cloudfront.net/2021/07/imagenes-sin-copyright-gratis.jpg" />
 
