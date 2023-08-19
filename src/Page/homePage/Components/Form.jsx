62a3864b6d7a952159cb2281a44aa979448a1403
amSebas <sebaslgr@gmail.com>
2023-08-18 22:06:31 -0500

    adding responsive of form and logic

--- src/Page/homePage/Components/Form.jsx
+++ src/Page/homePage/Components/Form.jsx
@@ -1,32 +1,40 @@
 import React from 'react'
+import { useForm } from "react-hook-form"
 
 
 const Form = () => {
+  const { register, formState:{ errors }, handleSubmit } = useForm();
 
-  function handleSubmit(e) {
-    
+  const onSubmit = (dataForm) => {
+    console.log(dataForm)
   }
-
   return (
     <section className='formulary'>
       <div className='grid-content'>
         <div className='text'>
-          <h1 className='white'>¡Tu voz importa y queremos escucharte!  </h1>
+          <h1 className='white'>¡Tu voz importa y </h1>
+          <h1 className='white'>queremos escucharte!</h1>
           <h1 className='purple'>Unete a nosotras</h1>
         </div>
-        <form className='grid-input' id='form' onSubmit={handleSubmit}>
+        <form className='grid-input' id='form' onSubmit={handleSubmit(onSubmit)}>
           <div className='content-input'>
-            <p>Tu nombre</p>
-            <input type="text" name='name' className='name' placeholder='Name' />
+            <p className='white'>Tu nombre</p>
+            <input type="text" className='name' {...register('name', {required: true, maxLength: 25,})} />
+            {errors.name?.type === 'required' && <p className='red'>Debes escribir tu nombre</p>}
+            {errors.name?.type === 'maxLength' && <p className='red'>Puedes escrbir maximo 25 caracteres</p>}
           </div>
           <div className='content-input'>
-            <p>Tu correo</p>
-            <input type="text" name='email' className='email' placeholder='Email' />
+            <p className='white'>Tu correo</p>
+            <input type="text" className='email' {...register('email', {required: true, pattern:/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/})}   />
+            {errors.email?.type === 'required' && <p className='red'>Debes escribir tu email</p>}
+            {errors.email?.type === 'pattern' && <p className='red'>Escribe un email valido</p>}
+
           </div>
           <div className='content-input'>
-            <p>Tu mensaje</p>
-            <textarea type="text" name='message' className='message' />
-            <button type='submit' className='btn-submit'>Enviar</button>
+            <p className='white-form-text'>Tu mensaje</p>
+            <textarea type="text" className='message'{...register('message', {required: true})} />
+            {errors.message?.type === 'required' && <p className='red'>Escribe un mensaje</p>}
+            <input type='submit' className='btn-submit' value='Enviar'/>
           </div>
         </form>
       </div>
