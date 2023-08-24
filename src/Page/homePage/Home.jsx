9df16ec18ef66b4b45397c1468559d5de0245996
Andrews Esteban <andrewsesteban7@gmail.com>
2023-08-23 20:36:17 -0500

    add books

--- src/Page/homePage/Home.jsx
+++ src/Page/homePage/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
       <Container/>
      <WaveAnimation/> 
       <Form/>
-
     </div>
   )
 }
--- src/Page/pageWave/Components/Books/MyBook.jsx
+++ src/Page/pageWave/Components/Books/MyBook.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
-import HTMLFlipBook from 'react-pageflip';
-import Page from './Page';
+import React, { useState } from 'react';
+import jsonData from '../../../../json_books.json';
 
+const MyBook = React.forwardRef((props, ref) => {
+  const [currentIndex, setCurrentIndex] = useState(0); // Inicialmente muestra el primer elemento
+  const totalItems = jsonData.length;
 
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalItems);
+  };
 
-const MyBook = () => {
-    const totalPages = 10;
-    const pages = [];
-    for (let pageNumber = 1; pageNumber <= totalPages; pageNumber++) {
-      pages.push(<Page key={pageNumber} number={pageNumber}>Page text {pageNumber}</Page>);
-    }
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + totalItems) % totalItems);
+  };
+
+  const currentItem = jsonData[currentIndex];
+
   return (
-    <div className='Book'>
-        <HTMLFlipBook 
-        width={300}
-        height={300}
-        // showCover={true}
-        >
-    {pages}
-        </HTMLFlipBook>
+    <div className='demoPage' ref={ref}>
+      <p>Título: {currentItem.Title}</p>
+      <p>Sinopsis: {currentItem.Synopsis}</p>
+      <p>Autor: {currentItem.Author}</p>
+      
+      <div className='Container'>
+      <button onClick={handlePrevious}>Anterior</button>
+        {currentItem.Flipbook !== 'Not_available' ? (
+          <iframe
+            className='Iframe'
+            title={`Iframe for ${currentItem.Title}`}
+            src={currentItem.Flipbook}
+            frameborder="0"
+            allowFullScreen="true">
+          </iframe>
+        ) : (
+          <a href={currentItem.Link} target="_blank" rel="noopener noreferrer">
+            <button>Archivo no disponible, accede a el aquí</button>
+          </a>
+        )}
+        <button onClick={handleNext}>Siguiente</button>
+      </div>
     </div>
-  )
-}
+  );
+});
 
-export default MyBook
+export default MyBook;
