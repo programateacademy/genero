1527252c35ab3fc5186a3b0232f9fb2e256d08c4
Andrews Esteban <andrewsesteban7@gmail.com>
2023-08-25 17:39:47 -0500

    styles

--- src/Page/pageWave/Components/Books/MyBook.jsx
+++ src/Page/pageWave/Components/Books/MyBook.jsx
@@ -1,63 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import BookSearch from './BookSearch';
+import React, { useState } from 'react';
+import girl from '../../../../assets/img/girlLoad.png'
 
 const MyBook = React.forwardRef((props, ref) => {
   const { books } = props;
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [filteredBooks, setFilteredBooks] = useState(books);
-
-  useEffect(() => {
-    setCurrentIndex(0); // Reinicia currentIndex cuando se cambian los resultados filtrados
-  }, [filteredBooks]);
 
   const handlePrevClick = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? filteredBooks.length - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? books.length - 1 : prevIndex - 1));
   };
 
   const handleNextClick = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === filteredBooks.length - 1 ? 0 : prevIndex + 1));
+    setCurrentIndex((prevIndex) => (prevIndex === books.length - 1 ? 0 : prevIndex + 1));
   };
 
-  const currentBook = filteredBooks[currentIndex];
+  const currentBook = books[currentIndex];
 
-  const handleSearch = (searchTerm) => {
-    const filteredResults = books.filter(
-      (book) =>
-        book.Title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        book.Author.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredBooks(filteredResults);
-  };
+  
 
   return (
     <div className='demoPage' ref={ref}>
-      <BookSearch onSearch={handleSearch} />
-      {filteredBooks.length > 0 ? (
-        <div>
-          <h4>Título: {currentBook.Title}</h4>
-          <p className='Text_author'>Autor: {currentBook.Author}</p>
-          <p className='Text_Synopsis'>Sinopsis: {currentBook.Synopsis}</p>
-          <div className='Container_book'>
-            {currentBook.Flipbook !== 'Not_available' ? (
-              <iframe
-                className='Iframe'
-                title={`Iframe for ${currentBook.Title}`}
-                src={currentBook.Flipbook}
-                frameBorder="0"
-                allowFullScreen="true"
-              ></iframe>
-            ) : (
-              <a href={currentBook.Link} target="_blank" rel="noopener noreferrer">
-                Archivo no disponible, accede a él aquí
-              </a>
-            )}
-          </div>
-          <button onClick={handlePrevClick}>Anterior</button>
-          <button onClick={handleNextClick}>Siguiente</button>
-        </div>
-      ) : (
-        <p>No se encontraron resultados</p>
-      )}
+ 
+      <h4>Título: {currentBook.Title}</h4>
+      <p className='Text_author'>Autor: {currentBook.Author}</p>
+      <p className='Text_Synopsis'>Sinopsis: {currentBook.Synopsis}</p>
+      <div className='Container_book'>
+      {currentBook.Flipbook !== 'Not_available' ? (
+            <iframe
+              className='Iframe'
+              title={`Iframe for ${currentBook.Title}`}
+              src={currentBook.Flipbook}
+              allowFullScreen="true"
+            ></iframe>
+          ) : (
+            <div className='Container_Load'>
+            <img className="Image_load" src={girl} alt="Imagen de carga"/>
+            <a href={currentBook.Link} target="_blank" rel="noopener noreferrer">Archivo no disponible, accede a él aquí</a>
+            </div>
+          )}
+      </div>
+      <button  className="button_books" onClick={handlePrevClick}>Anterior</button>
+      <button className="button_books" onClick={handleNextClick}>Siguiente</button>
+      
     </div>
   );
 });
