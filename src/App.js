31e7d0677bcbb1a32ed732109eef2af5ecf89057
bccastro <bhcastro1101@gmail.com>
2023-08-25 23:59:41 -0500

    responsive home design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import PageWave from './Page/pageWave/pageWave';
 import Home from './Page/homePage/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import DropdownMenu from './Page/DropdownMenu';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
         <nav>
-          <div className='Menu'>
-            <DropdownMenu />
+          <div className="Menu" >
+
+            <ul className="Navbar">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/pageWave/Cards-First-Wave/First-Wave-Books">Oleada 1</Link></li>
+              <li><Link to="/pageWave/Cards-Second-Wave/Second-Wave-Books">Oleada 2</Link></li>
+              <li><a href="/pageWave">Contacto</a></li>
+            </ul>
           </div>
         </nav>
         <Route path="/" exact component={Home} />
