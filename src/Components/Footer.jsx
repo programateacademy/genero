29c49f38ee3970976841f37fd9896b9d729181d9
JulianRodriguezV <juliancho7227@gmail.com>
2023-08-26 00:01:07 -0500

    cambios

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import './Footer.css';
+import logo_educamas from '../assets/img/Logo-educamas.png'
+
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <div className="sb_footer section_padding">
+                <div className="sb_footer-links">
+                    <div className="sb_footer-links_div">
+                        <h4>Recursos</h4>
+                        <a href="index.html">
+                            <p>Literatura</p>
+                        </a>
+                        <a href="index.html">
+                            <p>Historia</p>
+                        </a>
+                        <a href="index.html">
+                            <p>Videos</p>
+                        </a>
+                        <a href="index.html">
+                            <p>Imagenes</p>
+                        </a>
+                    </div>
+                    <div className="sb_footer-links_div">
+                        <h4>Acerca de</h4>
+                        <a href="index.html">
+                            <p>Feminismo</p>
+                        </a>
+                        <a href="index.html">
+                            <p>Temas de genero</p>
+                        </a>
+                        <a href="index.html">
+                            <p>Nous Cims Genero</p>
+                        </a>
+                    </div>
+                    <div className="sb_footer-links_div">
+                        <h4>Contacto</h4>
+                        <a href="index.html">
+                            <p>Politica de privacidad</p>
+                        </a>
+                        <a href="index.html">
+                            <p>Terminos de uso</p>
+                        </a>
+                    </div>
+                    <div className="sb_footer-links_div">
+                        <img src={logo_educamas}></img>
+                        <div className="wrapper">
+                            
+                        </div>
+                        {/*
+                        <div className="Socialmedia">
+                            <p><img src={fb} alt=""></img></p>
+                            <p><img src={twitter} alt=""></img></p>
+                            <p><img src={linkedlin} alt=""></img></p>
+                            <p><img src={Insta} alt=""></img></p>
+                        </div>
+                        */}                        
+                    </div>
+                </div>
+                <hr></hr>
+                <div className="sb_footer-below">
+                    <div className="sb_footer-copyright">
+                        <p>
+                            @{new Date().getFullYear()} Codeinn. All right reserved.
+                        </p>
+                    </div>
+                    <div className="sb_footer-below-links">
+                        <a href="index.html"><div><p>Terms & conditions</p></div></a>
+                        <a href="index.html"><div><p>Privacy</p></div></a>
+                        <a href="index.html"><div><p>Security</p></div></a>
+                        <a href="index.html"><div><p>Cookie Declaration</p></div></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- src/Page/pageWave/pageWave.jsx
+++ src/Page/pageWave/pageWave.jsx
@@ -3,15 +3,21 @@ import { useParams } from 'react-router-dom';
 import MyBook from './Components/Books/MyBook';
 import Card from '../pageWave/Components/Cards/Card';
 import jsonData from '../../data.json';
+import Footer from '../../Components/Footer';
 
 const Page_Wave = () => {
   const { cardsProp, booksProp } = useParams();
 
   return (
+    <body>
     <div>
       <Card jsonArray={jsonData[cardsProp]} />
       <MyBook books={jsonData[booksProp]} />
     </div>
+    <footer>
+      <Footer/>
+    </footer>
+    </body>
   );
 }
 
