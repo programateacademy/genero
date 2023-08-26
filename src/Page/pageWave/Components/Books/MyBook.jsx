ee61bfa995987fb23b045218b1279ce45ea035a2
Andrews Esteban <andrewsesteban7@gmail.com>
2023-08-25 21:06:36 -0500

    add filter

--- src/Page/pageWave/Components/Books/MyBook.jsx
+++ src/Page/pageWave/Components/Books/MyBook.jsx
@@ -1,51 +1,63 @@
 import React, { useState } from 'react';
-import girl from '../../../../assets/img/girlLoad.png'
+import girl from '../../../../assets/img/girlLoad.png';
+import BookDropdown from '../Books/BookDropdown '; 
 
 const MyBook = React.forwardRef((props, ref) => {
   const { books } = props;
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedBook, setSelectedBook] = useState(books[0]);
+  const [searchBy, setSearchBy] = useState('title'); // Predeterminado a búsqueda por título
 
-  const handlePrevClick = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? books.length - 1 : prevIndex - 1));
+  const handleBookSelect = (item, searchType) => {
+    const selected = searchType === 'title' ? books.find(book => book.Title === item) : books.find(book => book.Author === item);
+    setSelectedBook(selected);
   };
 
-  const handleNextClick = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === books.length - 1 ? 0 : prevIndex + 1));
+  const handleSearchByChange = (event) => {
+    setSearchBy(event.target.value);
   };
-
-  const currentBook = books[currentIndex];
-
-  
 
   return (
-    <div className='demoPage' ref={ref}>
- 
-      <h4>Título: {currentBook.Title}</h4>
-      <p className='Text_author'>Autor: {currentBook.Author}</p>
-      <p className='Text_Synopsis'>Sinopsis: {currentBook.Synopsis}</p>
-      <div className='Container_book'>
-      {currentBook.Flipbook !== 'Not_available' ? (
+    <div>
+      <div className='demoPage' ref={ref}>
+        <div>
+          <label htmlFor="searchBySelect">Buscar por:</label>
+          <select id="searchBySelect" value={searchBy} onChange={handleSearchByChange}>
+            <option value="title">Título</option>
+            <option value="author">Autor</option>
+          </select>
+        </div>
+        <div>
+          <BookDropdown books={books} onBookSelect={handleBookSelect} searchBy={searchBy} />
+        </div>
+        <h4>Título: {selectedBook.Title}</h4>
+        <p className='Text_author'>Autor: {selectedBook.Author}</p>
+        <p className='Text_Synopsis'>Sinopsis: {selectedBook.Synopsis}</p>
+        <div className='Container_book'>
+          {selectedBook.Flipbook !== 'Not_available' ? (
             <iframe
               className='Iframe'
-              title={`Iframe for ${currentBook.Title}`}
-              src={currentBook.Flipbook}
-              allowFullScreen="true"
+              title={`Iframe for ${selectedBook.Title}`}
+              src={selectedBook.Flipbook}
+              allowFullScreen={true}
             ></iframe>
           ) : (
             <div className='Container_Load'>
-            <img className="Image_load" src={girl} alt="Imagen de carga"/>
-            <a href={currentBook.Link} target="_blank" rel="noopener noreferrer">Archivo no disponible, accede a él aquí</a>
+              <img className="Image_load" src={girl} alt="Imagen de carga"/>
+              <a href={selectedBook.Link} target="_blank" rel="noopener noreferrer">
+                Archivo no disponible, accede a él aquí
+              </a>
             </div>
           )}
+        </div>
       </div>
-      <button  className="button_books" onClick={handlePrevClick}>Anterior</button>
-      <button className="button_books" onClick={handleNextClick}>Siguiente</button>
-      
     </div>
   );
 });
 
 export default MyBook;
+
+
+
 
 
 
