26b456074221e85814181cc20dcb7b71b532dd94
JulianRodriguezV <juliancho7227@gmail.com>
2023-08-28 22:59:33 -0500

    Adjusted navbar styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -24,7 +24,7 @@ class Navbar extends Component {
                     <ul id="Navbar" className={this.state.clicked ? "#Navbar active" : "#Navbar"}>
                         <li><a className="active" href="/"> Inicio</a></li>
                         <li><Link to="/#about" smooth={true}>Sobre</Link></li>
-                        <li><a href="/pageWave/Cards-First-Wave/First-Wave-Books">Recursos</a></li>
+                        <li><a href="/pageWave/Cards-First-Wave/First-Wave-Books">Recursos </a><i className='fas fa-caret-down' /></li>
                         {/*<li><a href="/pageWave/Cards-First-Wave/First-Wave-Books">Oleada 1</a></li>
                         <li><a href="/pageWave/Cards-Second-Wave/Second-Wave-Books">Oleada 2</a></li>*/}
                         <li><Link to="/#Footer" smooth={true}>Contacto</Link></li>
