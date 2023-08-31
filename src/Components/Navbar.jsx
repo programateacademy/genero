abc6c8f3ffdc809b53f7a05cff3cbac127273173
wkarol <122057880+karolgalindo02@users.noreply.github.com>
2023-08-30 20:57:40 -0500

    fix navbar and carousel, animation button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,42 +1,84 @@
 import { Component } from "react";
 import "./NavbarStyles.css";
-import { HashLink as Link } from 'react-router-hash-link';
-import photo from '../assets/img/imagenav.jpeg'
-// import { findRenderedDOMComponentWithClass } from "react-dom/test-utils";
+import { HashLink as Link } from "react-router-hash-link";
+import photo from "../assets/img/imagenav.png";
+
 
 class Navbar extends Component {
-    
-    state={clicked: false}
-    handleClick = ()=>{
-        this.setState({clicked:!this.state.clicked})
+  state = { clicked: false, scrolled: false };
 
-    }
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
 
-    scrollToBottom = () => {
-        const element = document.getElementById('bottom-section'); // Cambia 'bottom-section' por el ID de tu sección inferior
-        element.scrollIntoView({ behavior: 'smooth' })
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    // Verifica la posición del scroll, por ejemplo, a 100 píxeles desde arriba
+    if (window.scrollY > 100) {
+      this.setState({ scrolled: true });
+    } else {
+      this.setState({ scrolled: false });
     }
-    render () {
+  };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  scrollToBottom = () => {
+    const element = document.getElementById("bottom-section");
+    element.scrollIntoView({ behavior: "smooth" });
+  };
+  render() {
     return (
-        <>
-            <nav>
-                <div>
-                    <ul id="Navbar" className={this.state.clicked ? "#Navbar active" : "#Navbar"}>
-                        <li><a className="active" href="/"> Inicio</a></li>
-                        <li><Link to="/#about" smooth={true}>Sobre</Link></li>
-                        <li><a href="/pageWave/Cards-First-Wave/First-Wave-Books">Oleada 1</a></li>
-                        <li><a href="/pageWave/Cards-Second-Wave/Second-Wave-Books">Oleada 2</a></li>
-                        <li><Link to="/#Footer" smooth={true}>Contacto</Link></li>
-                    </ul>
-                    
-                </div>
-                <div id="Mobile" onClick={this.handleClick}>
-                    <i id="Bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-                </div>
-            </nav>
-            <img id="photonav" src={photo} alt="foto_menu" />
-        </>
-    )
-}
+      <>
+        <nav>
+          <div>
+            <ul
+              id="Navbar"
+              className={this.state.clicked ? "#Navbar active" : "#Navbar"}
+            >
+              <li>
+                <a className="active" href="/">
+                  {" "}
+                  Inicio
+                </a>
+              </li>
+              <li>
+                <Link to="/#about" smooth={true}>
+                  Sobre
+                </Link>
+              </li>
+              <li>
+                <a href="/pageWave/Cards-First-Wave/First-Wave-Books">
+                  Oleada 1
+                </a>
+              </li>
+              <li>
+                <a href="/pageWave/Cards-Second-Wave/Second-Wave-Books">
+                  Oleada 2
+                </a>
+              </li>
+              <li>
+                <Link to="/#Footer" smooth={true}>
+                  Contacto
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div id="Mobile" onClick={this.handleClick}>
+            <i
+              id="Bar"
+              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
+        </nav>
+        <img id="photonav" src={photo} alt="foto_menu" className={this.state.scrolled ? "hidden" : ""}/>
+      </>
+    );
+  }
 }
 export default Navbar;
--- src/Page/homePage/Components/Section/WaveAnimation.jsx
+++ src/Page/homePage/Components/Section/WaveAnimation.jsx
@@ -1,26 +1,24 @@
 import React from "react";
 
-
-const WaveAnimation = ({waveTitle}) => {
+const WaveAnimation = ({ waveTitle }) => {
   return (
-<div class="rotate">
-  <div class="wave-effect wave-anim">
-    <div class="waves-shape shape-one">
-      <div class="wave wave-one"></div>
+    <div>
+      <div>
+        <div class="section-title text-center">OLEADA 1</div>
+        <div class="section-title-border mt-3"></div>
+      </div>
+      <div class="wave-effect wave-anim">
+        <div class="waves-shape shape-one">
+          <div class="wave wave-one"></div>
+        </div>
+        <div class="waves-shape shape-two">
+          <div class="wave wave-two"></div>
+        </div>
+        <div class="waves-shape shape-three">
+          <div class="wave wave-three"></div>
+        </div>
+      </div>
     </div>
-    <div class="waves-shape shape-two">
-      <div class="wave wave-two"></div>
-    </div>
-    <div class="waves-shape shape-three">
-      <div class="wave wave-three"></div>
-    </div>
-  </div>
-  <div>
-  <div class="section-title text-center">OLEADA 1</div>
-  <div class="section-title-border mt-3"></div>
-  </div>
-</div>
-
   );
 };
 export default WaveAnimation;
