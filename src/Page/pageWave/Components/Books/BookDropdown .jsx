379fc86695005866854019b9859bc059f707626f
Andrews Esteban <andrewsesteban7@gmail.com>
2023-09-01 17:21:59 -0500

    merge finish

--- src/Page/pageWave/Components/Books/BookDropdown .jsx
+++ src/Page/pageWave/Components/Books/BookDropdown .jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import '../../styles.css'
 
 const BookDropdown = ({ books, onBookSelect, searchBy }) => {
   const [selectedItem, setSelectedItem] = useState(
--- src/Page/pageWave/Components/Cards/Card.jsx
+++ src/Page/pageWave/Components/Cards/Card.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../../styles.css';
+
 
 function Card(props) {
     const { jsonArray } = props;
