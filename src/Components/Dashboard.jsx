2ca6f9d1acba8b2b9d06e5dddcd6cd11d280bbd2
santiago <san.hernandez097@gmail.com>
2023-12-18 21:54:25 -0500

    Added footer to the dashboard, corrected spelling in the parent component and fixed the full-screen display

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -13,11 +13,12 @@ import Tittle from "../Page/dashboardComponents/Tittle/Tittle";
 import TotalAttention from "../Page/dashboardComponents/TotalAttention/TotalAttention";
 import YearButtons from "../Page/dashboardComponents/YearButtons/YearButtons";
 import Data from '../assets/Services/Data/Data.json';
+import Footer from "./Footer";
 
 const Dashboard = () => {
     const [selectedAño, setselectedAño] = useState(null);
     const [selectedAñoContent, setselectedAñoContent] = useState([]);
-    console.log(selectedAño) // se realiza el console para llamar la función
+    console.log(selectedAño) // The console is used to call the function
     const handleSelectedAño = (item) => {
         setselectedAño(item);
         const selectedYearData = Data.find((data) => data.año === item);
@@ -31,63 +32,67 @@ const Dashboard = () => {
             <div className="container-tittle">
             <Tittle />
             </div>
+
             <div className="column" >            
-            
-            <br></br>
-            <br></br>
-            <br></br>
-            <TotalAttention />
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <YearButtons onYearSelect={handleSelectedAño} jsonData={Data} />
-            <br></br>
-            <di className="container-graphics">
-            <Graphics selectedAñoContent={selectedAñoContent}/>
-            <br></br>            
-            </di>
-            <div className="container-Secondgraphics">
-            <SecondGraphic selectedAñoContent={selectedAñoContent}/> 
-            </div>           
-            <br></br>
-            <div className="container-BoardGraphic">
-            <BoardGraphic selectedAñoContent={selectedAñoContent}/>
-            </div>
-            <br></br>
-            <SecondBoardGraphic />
-            <br></br>
-            <div className="container-Sex">
-            <SexGraphic selectedAñoContent={selectedAñoContent} />
+                <br></br>
+                <br></br>
+                <br></br>
+                <TotalAttention />
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <YearButtons onYearSelect={handleSelectedAño} jsonData={Data} />
+                <br></br>
 
-            <div className="container-SexualOrientation">
-            <br></br>
-            <SexualOrientationGraphic selectedAñoContent={selectedAñoContent} />
-            </div>        
-            </div>
-            <div className="container-Identity">
-            <br></br>
-            <IdentityGraphic selectedAñoContent={selectedAñoContent}/>
-            <br></br>
-            <div>
+                <div className="container-graphics">
+                    <Graphics selectedAñoContent={selectedAñoContent}/>
+                    <br></br>            
+                </div>
+
+                <div className="container-Secondgraphics">
+                    <SecondGraphic selectedAñoContent={selectedAñoContent}/> 
+                </div>   
+
+                <br></br>
+
+                <div className="container-BoardGraphic">
+                    <BoardGraphic selectedAñoContent={selectedAñoContent}/>
+                </div>
+
+                <br></br>
+                <SecondBoardGraphic />
+                <br></br>
+
+                <div className="container-Sex">
+                    <SexGraphic selectedAñoContent={selectedAñoContent} />
+
+                    <div className="container-SexualOrientation">
+                        <br></br>
+                        <SexualOrientationGraphic selectedAñoContent={selectedAñoContent} />
+                    </div>   
+
+                </div>
+
+                <div className="container-Identity">
+                    <br></br>
+                    <IdentityGraphic selectedAñoContent={selectedAñoContent}/>
+                    <br></br>
+                <div>
                 
-            </div>
-            <div className="container-Condition">
-            <ConditionGraphic selectedAñoContent={selectedAñoContent} />
+                </div>
+
+                <div className="container-Condition">
+                    <ConditionGraphic selectedAñoContent={selectedAñoContent} />
+                </div>
+                
+            </div>  
 
             </div>
-             </div>
-            <br></br>   
 
-            </div>
-
-            
-                     
-            <Download />         
-                      
-                        
-                        
-                        
+            <Download /> 
+            <br></br>
+            <Footer />        
         </div>
     );
 };
